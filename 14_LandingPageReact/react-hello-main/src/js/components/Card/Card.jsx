c102cb4780776795d735f55d2e74e3e39523ab02
Shaznay Sison <82836907+Shaznay-S@users.noreply.github.com>
2023-11-07 10:55:47 -0500

    edited a few projects

--- 14_LandingPageReact/react-hello-main/src/js/components/Card/Card.jsx
+++ 14_LandingPageReact/react-hello-main/src/js/components/Card/Card.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './card.css';
 
-const Card = ({ title, text }) => {
+const Card = ({ title, text, imageUrl }) => {
     return (
       <div className="card">
+        <img src={imageUrl} alt={title} className="card-image" />
         <div className="card-body">
           <h3 className="card-title">{title}</h3>
           <p className="card-text">{text}</p>
+        </div>
+        <div className="card-footer">
+            <button className="btn-primary">Find out more!</button>
         </div>
       </div>
     );
--- 14_LandingPageReact/react-hello-main/src/js/components/home.jsx
+++ 14_LandingPageReact/react-hello-main/src/js/components/home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from './Navbar/Navbar';
 import Jumbotron from './Jumbotron/Jumbotron';
 import Card from './Card/Card';
+import Footer from './Footer/Footer';
 
 //create your first component
 const Home = () => {
@@ -10,11 +11,12 @@ const Home = () => {
 			<Navbar />
 			<Jumbotron />
 			<div className="cards-container">
-				<Card title="Card Title 1" text="Some quick example text to build on the card title." />
-        		<Card title="Card Title 2" text="Some more example text to build on the card title." />
-        		<Card title="Card Title 3" text="Further example text to build on the card title." />
-				<Card title="Card Title 4" text="Further example text to build on the card title." />
+				<Card title="Card Title 1" text="Some quick example text to build on the card title." imageUrl="https://fastly.picsum.photos/id/40/4106/2806.jpg?hmac=MY3ra98ut044LaWPEKwZowgydHZ_rZZUuOHrc3mL5mI" />
+        		<Card title="Card Title 2" text="Some more example text to build on the card title." imageUrl="https://fastly.picsum.photos/id/41/1280/805.jpg?hmac=W9CWeYdlZisqEfhjuODl83T3lCXAqjUZrOe9iMFPYmI"/>
+        		<Card title="Card Title 3" text="Further example text to build on the card title." imageUrl="https://fastly.picsum.photos/id/43/1280/831.jpg?hmac=glK-rQ0ppFClW-lvjk9FqEWKog07XkOxJf6Xg_cU9LI"/>
+				<Card title="Card Title 4" text="Further example text to build on the card title." imageUrl="https://fastly.picsum.photos/id/57/2448/3264.jpg?hmac=ewraXYesC6HuSEAJsg3Q80bXd1GyJTxekI05Xt9YjfQ"/>
 			</div>
+			<Footer />
 		</div>
 	);
 };
